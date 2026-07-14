e7b42b87bcdf966f49d0b821441ff2ead6d6721e
Tech Pranav <techpranav21@gmail.com>
2026-07-14 23:34:35 +0530

    Loop Statement

---
+++ Loop/PrintNumbers.java
@@ -0,0 +1,13 @@
+package Loop;
+
+public class PrintNumbers {
+    public static void main() {
+
+        // Using For Loop
+        System.out.println("1 to 10 Number :-");
+
+        for(int i=1 ; i<=10 ; i++){
+           System.out.println(i);
+        }
+    }
+}
